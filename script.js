899051a6640dcba90d5d0d34004c353f1fb093a5
Trey Wayland <waylandtrey7@gmail.com>
2024-06-15 12:23:07 -0700

    Refactoring out the collect Employees function.

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
+
+
+const promptUserData_firstName = function() {
+  let userFirstName = prompt("First Name"); // prompts the user for first name
+  // let userLastName = prompt("Last Name"); // prompts the user for last name
+  // let userSalary = prompt("Salary"); // prompts the user for salary
+  return userFirstName;
+}
+
+const promptUserData_lastName = function() {
+  // let userFirstName = prompt("First Name"); // prompts the user for first name
+  let userLastName = prompt("Last Name"); // prompts the user for last name
+  // let userSalary = prompt("Salary"); // prompts the user for salary
+  return userLastName;
+}
+
+const promptUserData_salary = function() {
+  // let userFirstName = prompt("First Name"); // prompts the user for first name
+  // let userLastName = prompt("Last Name"); // prompts the user for last name
+  let userSalary = prompt("Salary"); // prompts the user for salary
+  return userSalary;
+}
+
+const create_employeeObject = function() {
+    // let promptFirstName = promptUserData_firstName();
+
+  const employeeData = {
+    firstName : promptUserData_firstName(),
+    lastName : promptUserData_lastName(),
+    salary : promptUserData_salary()
+  };
+
+  return employeeData;
+}
+
+
+
 // Collect employee data
 const collectEmployees = function() {
 
-  let addAnotherDecision; // needed to initate prompting the user
+  let addAnotherEmployee = true; // needed to initate prompting the user
   let userArray = []; // this stores the employee object and will be returned
 
   
-  while(addAnotherDecision){
-    let userFirstName = prompt("First Name"); // prompts the user for first name
-    let userLastName = prompt("Last Name"); // prompts the user for last name
-    let userSalary = prompt("Salary"); // prompts the user for salary
+  while(addAnotherEmployee){
+
+    // promptUserData_firstName();
+
 
     // stores information into an object
-    const employeeData = {
-      firstName : userFirstName,
-      lastName : userLastName,
-      salary : userSalary
-    };
+    // const employeeData = {
+    //   firstName : userFirstName,
+    //   lastName : userLastName,
+    //   salary : userSalary
+    // };
   
-    userArray.push(employeeData); // pushes that object onto the array
+    userArray.push(create_employeeObject()); // pushes that object onto the array
 
-    addAnotherDecision = confirm("Add another employee?"); // prompt the user to add another user
+    addAnotherEmployee = confirm("Add another employee?"); // prompt the user to add another user
     
-    if (addAnotherDecision === false){ // returns the array if the user doesn't want to add another user
+    if (addAnotherEmployee === false){ // returns the array if the user doesn't want to add another user
       return userArray;
     }
   }
@@ -33,7 +70,7 @@ const collectEmployees = function() {
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
-  
+
   let numberOfEmployees = employeesArray.length;
   let runningTotalEmployeeSalary = 0;
   for (let i = 0; i < numberOfEmployees; i++){
@@ -43,12 +80,15 @@ const displayAverageSalary = function(employeesArray) {
   console.log(`The average emloyee salary between our ${numberOfEmployees} employee(s) is $` + averageSalary);
 }
 
+
+
+
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   let numberOfEmployees = employeesArray.length;
   let randomNumber = Math.floor(Math.random() * numberOfEmployees);
   let randomEmployee = employeesArray[randomNumber];
-  console.log(`Congratulation to  ${randomEmployee.userFirstName} ${randomEmployee.lastName}, our random drawing winner!`);
+  console.log(`Congratulation to  ${randomEmployee.firstName} ${randomEmployee.lastName}, our random drawing winner!`);
 }
 
 /*
